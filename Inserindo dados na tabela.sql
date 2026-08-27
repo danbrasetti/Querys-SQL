d07124e72006fbe2944d97897900000093940139
danbrasetti <brasetti93@gmail.com>
2026-08-27 17:51:43 -0300

    Query das criações das tabelas do Serenatto

---
+++ Inserindo dados na tabela.sql
@@ -0,0 +1,14 @@
+USE serenatto;
+
+INSERT INTO Colaboradores (ID_colaborador, Nome_colaborador, Cargo_colaborador, Data_Contratacao, Telefone_colaborador, Email_colaborador, Rua_colaborador, Bairro_colaborador, Cidade_colaborador, Estado_colaborador, CEP_colaborador) 
+VALUES
+(2, 'Marta Sousa', 'Chef de Cozinha', '2022-05-10', '215555678', 'marta.sousa@email.com', 'Rua dos Sabores - 456', 'Saboroso', 'São Paulo', 'SP', '20000001'),
+(3, 'Pedro Almeida', 'Barista', '2022-07-20', '315557890', 'pedro.almeida@email.com', 'Avenida do Café - 789', 'Centro', 'São Paulo', 'SP', '01000001'),
+(4, 'Sofia Rodrigues', 'Garçom', '2022-01-12', '415552345', 'sofia.rodrigues@email.com', 'Rua da Hospitalidade - 101', 'Boas Maneiras', 'São Paulo', 'SP', '40000001'),
+(5, 'João Pereira', 'Atendente', '2022-09-05', '515558765', 'joao.pereira@email.com', 'Rua das flores - 210', 'Centro', 'São Paulo', 'SP', '20000001'),
+(6, 'Inês Lima', 'Barista', '2022-04-02', '615553421', 'ines.lima@email.com', 'Rua das Entregas - 280', 'Entregas Rápidas', 'São Paulo', 'SP', '90000001'),
+(7, 'Antonio Artur', 'Entregador', '2023-01-10', '568442577', 'antonio.artur@email.com', 'Rua das flores - 210', 'Centro', 'São Paulo', 'SP', '20000001');
+
+
+
+
