92f1c23dd16b036840a1ea532728a3aa0cb990bd
danbrasetti <brasetti93@gmail.com>
2026-08-27 19:52:59 -0300

    Criação das Tabelas do Serenatto e Inserção dos dados

--- Criação das tabelas.sql
+++ Criação das tabelas.sql
@@ -1,62 +1,70 @@
-CREATE DATABASE Serenatto;
-USE Serenatto;
+CREATE DATABASE serenatto;
+USE serenatto;
 
-CREATE TABLE Produtos (
-ID_produto INT PRIMARY KEY,
-Nome_produto VARCHAR (50),
-Descricao_produto VARCHAR (20),
-Preco_produto DECIMAL (10,2),
-Categoria_produto VARCHAR (20)
+CREATE TABLE produtos (
+  id INT PRIMARY KEY,
+  nome VARCHAR(255),
+  descricao VARCHAR(255),
+  preco DECIMAL(10,2),
+  categoria VARCHAR(50)
 );
 
-CREATE TABLE Clientes (
-ID_cliente INT PRIMARY KEY NOT NULL,
-Nome_cliente VARCHAR (30),
-Telefone_cliente VARCHAR (15),
-Email_cliente VARCHAR (50) DEFAULT 'Sem email',
-Endereco_cliente VARCHAR (30)
+CREATE TABLE colaboradores (
+    ID INT PRIMARY KEY ,
+    Nome VARCHAR(255) NOT NULL,
+    Cargo VARCHAR(100),
+    DataContratacao DATE,
+    Telefone VARCHAR(20),
+    Email VARCHAR(100),
+	Rua VARCHAR(100) NOT NULL,
+	Bairro VARCHAR(100) NOT NULL,
+	Cidade VARCHAR(100) NOT NULL,
+	Estado VARCHAR(2) NOT NULL,
+	cep VARCHAR(8) NOT NULL
 );
 
-CREATE TABLE Pedidos (
-ID_pedido INT PRIMARY KEY NOT NULL,
-ID_cliente INT NOT NULL,
-Datahora_pedido DATETIME,
-Status_pedido VARCHAR (50),
 
-FOREIGN KEY (ID_cliente) REFERENCES Clientes (ID_cliente) ON DELETE CASCADE
+
+CREATE TABLE fornecedores (
+    ID INT PRIMARY KEY ,
+    Nome VARCHAR(255) NOT NULL,
+  	Contato VARCHAR(100) NOT NULL,
+    Telefone VARCHAR(20),
+    Email VARCHAR(100),
+	Rua VARCHAR(100) NOT NULL,
+	Bairro VARCHAR(100) NOT NULL,
+	Cidade VARCHAR(100) NOT NULL,
+	Estado VARCHAR(2) NOT NULL,
+	cep VARCHAR(8) NOT NULL
 );
 
-CREATE TABLE Colaboradores (
-ID_colaborador INT PRIMARY KEY NOT NULL,
-Nome_colaborador VARCHAR (50),
-Cargo_Colaborador VARCHAR (20),
-Data_contratacao DATE,
-Telefone_colaborador VARCHAR (15),
-Email_colaborador VARCHAR (50),
-Rua_colaborador VARCHAR (255),
-Bairro_colaborador VARCHAR (20),
-Cidade_colaborador VARCHAR (20),
-Estado_colaborador VARCHAR (20),
-Cep_colaborador CHAR (8) NOT NULL
+CREATE TABLE clientes (
+  id INT NOT NULL,
+  nome VARCHAR(255),
+  telefone VARCHAR(20),
+  email VARCHAR(100) DEFAULT 'Sem email',
+  endereco VARCHAR(255),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE Fornecedores (
-ID_fornecedor INT PRIMARY KEY NOT NULL,
-Nome_fornecedor VARCHAR (30),
-Contato_fornecedor VARCHAR (30),
-Telefone_Fornecedor VARCHAR (15),
-Email_Fornecedor VARCHAR (50)
+CREATE TABLE pedidos (
+  id INT PRIMARY KEY,
+  idcliente INT,
+  datahorapedido DATETIME,
+  status VARCHAR(50),
+  FOREIGN KEY (idcliente) REFERENCES clientes(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Itenspedidos(
-ID_pedido INT,
-ID_produto INT,
-Quantidade_pedido INTEGER,
-Preco_unitario DECIMAL (10,2),
-PRIMARY KEY(ID_pedido,ID_produto),
-
-FOREIGN KEY (ID_pedido) REFERENCES Pedidos (ID_pedido) ON DELETE CASCADE,
-FOREIGN KEY (ID_produto) REFERENCES Produtos (ID_produto) ON DELETE CASCADE
-
-
+CREATE TABLE itenspedidos (
+  idpedido INT,
+  idproduto INT,
+  quantidade INTEGER,
+  precounitario DECIMAL(10,2),
+  PRIMARY KEY (idpedido,idproduto),
+  FOREIGN KEY (idpedido) REFERENCES pedidos(id) ON DELETE CASCADE,
+  FOREIGN KEY (idproduto) REFERENCES produtos(id) ON DELETE CASCADE
 );
+
+
+
+
